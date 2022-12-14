e60b38706f7aa0252aded9565fceff288b703c5f
JR Guerra <jrwarg@gmail.com>
2022-12-14 16:44:23 -0300

    Criando pastas para estudos MS Learning C#

--- Estudos C# paralelos/C#MS-101/Program.cs
+++ Estudos C# paralelos/C#MS-101/Program.cs
@@ -4,8 +4,78 @@ Console.Write("Congratulations!");
 Console.Write(" ");
 Console.Write("You wrote your first lines of code!"); */
 
-Console.WriteLine("This is the first line.");
+/* Console.WriteLine("This is the first line.");
 Console.Write("This is ");
-Console.Write("the second line.");
+Console.Write("the second line."); */
+
+/* Literais 
+Console.WriteLine('b');
+// Console.WriteLine('Hello World!'); // Too many chars
+Console.WriteLine(123);
+Console.WriteLine(12.30m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
+*/
+/* Variáveis */
+/* string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Beth";
+Console.WriteLine(firstName);
+firstName = "Conrad";
+Console.WriteLine(firstName);
+firstName = "Grant";
+Console.WriteLine(firstName); */
+
+/* // Variáveis do tipo implícito //
+var message = "Hello World!"; */
+
+/* string name = "Bob";
+int messages = 3;
+double deg = 34.4;
+
+Console.WriteLine($"Hello, {name} You have {messages} messages in your inbox. The temperature is {deg} celsius."); */
+
+/*  Characteres Escape sequences and Verbatim Strings  */
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+
+Console.WriteLine("Hello \"World\"!");
+
+Console.WriteLine("c:\\source\\repos");
+
+Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+// Kon'nichiwa World
+Console.WriteLine("\n\n\u3053\u3093\u306B\u3061\u306F World!");
+
+
+/* Console.WriteLine(@" c:\source\repos 
+    (""this is where your code goes"")"); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
