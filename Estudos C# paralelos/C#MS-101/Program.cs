fda7d6ec86ede8c00dd65bfaed9352acc2d96cb5
JR Guerra <jrwarg@gmail.com>
2022-12-16 14:49:04 -0300

    Ultimo commit - as próximas lições serão feitas no editor online no Learn MS Training

--- Estudos C# paralelos/C#MS-101/Program.cs
+++ Estudos C# paralelos/C#MS-101/Program.cs
@@ -43,7 +43,7 @@ double deg = 34.4;
 
 Console.WriteLine($"Hello, {name} You have {messages} messages in your inbox. The temperature is {deg} celsius."); */
 
-/*  Characteres Escape sequences and Verbatim Strings  */
+/*  Characteres Escape sequences and Verbatim Strings  
 Console.WriteLine("Hello\nWorld!");
 Console.WriteLine("Hello\tWorld!");
 
@@ -63,6 +63,27 @@ Console.WriteLine("\n\n\u3053\u3093\u306B\u3061\u306F World!");
 
 /* Console.WriteLine(@" c:\source\repos 
     (""this is where your code goes"")"); */
+
+/* String concatenation 
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+
+/* String interpolation 
+Console.WriteLine($"{greeting} {firstName}!");
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+Console.WriteLine($"C:\\Output\\{projectName}\\Data");*/
+
+// Exercise
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+            
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
 
 
