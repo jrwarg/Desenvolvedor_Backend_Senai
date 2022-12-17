459fc40994bcc19461f83d44444e2e996cb141e4
JR Guerra <jrwarg@gmail.com>
2022-12-17 11:04:41 -0300

    acréscimo de novas lições IF Statements e Arrays

--- Estudos C# paralelos/C#MS-101/Program.cs
+++ Estudos C# paralelos/C#MS-101/Program.cs
@@ -108,11 +108,160 @@ Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}"); */
 
 // Exercise //
-int firstValue = 500;
+/* int firstValue = 500;
 int secondValue = 600;
 int largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine(largerValue);
+Console.WriteLine(largerValue); */
+
+// STATEMENT IF //
+
+//Random dice = new Random();
+
+/* int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if((roll1 == roll2) && (roll2 == roll3)){
+    Console.WriteLine("You rolled triples! +6 bonus to total");
+    total += 6;
+}
+
+if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+    Console.WriteLine("You rolled doubles! + 2 bonus to total!");
+    total += 2;
+}
+
+if (total >= 15){
+    Console.WriteLine("You win!!");
+}
+else {
+    Console.WriteLine("Sorry, you lose.");
+}; */
+
+/* string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
+*/
+/* int roll1 = 6;
+int roll2 = 6;
+int roll3 = 5;
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
+if (total > 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else 
+{
+    Console.WriteLine("You win a kitten");
+} */
+
+// Exercise //
+/* Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+if (daysUntilExpiration < 1) {
+    Console.WriteLine("Your subscription has expired!");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+} */
+
+// ARRAYS //
+/*string [] fraudulentOrderIDs = new string[3];
+
+ fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789"; */
+/* string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+string[] names = {"Bob", "Conrad", "Grant"};
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+ */
+/* 
+int[] inventory = {200, 450, 700, 175, 250};
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory"); */
+
+// Exercise //
+string[] fakeOrdersIDs = {"B123", "C234", "D345", "C15", "B177", "G3003", "C235", "B179"};
+foreach (string items in fakeOrdersIDs)
+{
+    if (items.StartsWith("B"))
+    {
+        Console.WriteLine(items);
+    }
+}
+
+
+
+
+
+
 
 
 
